85d68655db7de3ed8ff2d0983a7b38670a999f90
tarunreddykavadi <tarunreddy.kavadi.79@gmail.cpm>
2024-04-30 03:19:57 +0100

    work

--- src/Pages/Faqs.jsx
+++ src/Pages/Faqs.jsx
@@ -3,43 +3,96 @@ import styled from 'styled-components'
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import { useState } from 'react';
 import { FaqData } from '../data';
+import bgImage from '../assets/image15.jpeg'
+
+
+ const BgImage = styled.div`
+      height:20rem;
+     max-width:100vw;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     background-image: url(${bgImage});
+     background-size: 75rem 40rem;
+     background-position: center;
+     background-repeat: no-repeat;
+     transition: background-color 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+ `;
+ const Overlay = styled.div`
+     display: flex;
+    justify-content: center;
+    align-items: center;
+    height:20rem;
+     width:100vw;
+     background-color: black;
+     opacity: 0.5;
+     text-align: center;
+  
+     h1{
+         color: white;
+         font-size: 3rem;
+     }
+`;
+
+const SubHeading = styled.div`
+ display:flex;
+ flex-direction: column;
+ align-items: center;
+ margin-bottom:2rem;
+ overflow: hidden;
+
+`;
 const Details = styled.details`
 display:flex;
+margin: 1rem 0rem 1rem 5rem; 
 .arrow{
-    margin-left:30rem;
+    position: absolute;
+    margin-left:50rem;
 }
 `;
 const Box = styled.div`
      display: flex;
-     width: 49.9rem;
-     border: 1px solid black;  
+     margin-left: 6rem;
+     justify-content: center;
+     width: 45rem;
+     padding:2rem;  
+     background-color: whitesmoke;
 `;
 
 
 const Faqs = ()=>{
 const [isOpen,setIsOpen] =useState({})
  return <Layout>
+    <BgImage><Overlay><h1>FAQ'S</h1></Overlay></BgImage>
+
+     
+         <p style={{lineHeight:'2rem',padding:'4rem'}}>Welcome to the Universal Painters FAQ page! Here, you’ll find detailed answers to the most frequently asked questions (FAQs) about our services, products, and processes. We’ve gathered this information to ensure you have a clear and comprehensive understanding of how we can transform your residential, commercial, or industrial property with our expert painting and coating solutions. Please read FAQs below to find helpful insights and tips tailored to your needs, and if you have any further queries, we’re just a call or email away!</p>
+     <SubHeading>
+        <h1 style={{fontSize:'2.5rem'}}>Frequently Asked Questions (FAQ)</h1>
+    </SubHeading>
+
    {FaqData.map((props)=>{
     return  <Details key={props.id} open={!isOpen[props.id]}>
         <summary style={{ listStyle: 'none' }}>
             <a onClick={() => setIsOpen({[props.id]:!isOpen[props.id]} )} style={{
-                color: 'black',
+                color: `${isOpen[props.id] ? 'white' : 'black'}`,
                 display: 'flex',
                 alignItems: 'center',
                 position: 'relative',
                 padding: '0.5rem ',
                 textDecoration: 'none',
                 zIndex: '1',
-                height: '2.5rem'
-            }}>
-                <h3 >{props.question}</h3>
+                height: '2.5rem',
+                        }}>
+                <h3>{props.question}</h3>
                 <ArrowUpwardIcon className='arrow' style={isOpen[props.id] ? { transform: 'rotate(180deg)', transition: '0.2s' } : { transform: 'rotate(0deg)', transition: '0.2s' }}> </ArrowUpwardIcon>
                 <svg
                     style={{
                         position: 'absolute',
                         top: '0',
                         left: '0',
-                        zIndex: '-1'
+                        zIndex: '-1',
+                        
                     }}
                     x="0px"
                     y="0px"
@@ -51,7 +104,7 @@ const [isOpen,setIsOpen] =useState({})
                         className='path'
                         pointerEvents='all'
                         stroke='red'
-                        fill='transparent'
+                        fill={isOpen[props.id] ? '#add8e6' : 'transparent'}
                         points='85,55 81,55 51,55 42.5,64 34,55 0,55 0,0 34.4,0 42.5,9.5 50.6,0 81,0 85,0 1000,0 1000,55'
                     >
                     </polygon>
